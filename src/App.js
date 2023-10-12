a6a4245090803bd370141353bf398a6c22459fa3
Kobby7777 <godfredmirekuowusu@gmail.com>
2023-10-12 10:58:19 +0100

    NavBar Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+// import { Hero } from './Components/Hero/Hero';
+import { NavBar } from './Components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='relative w-full'>
+        <img src='./img/bg.png' alt='hero'/>
+      <NavBar />
+      </div>
+      {/* <Hero /> */}
     </div>
   );
 }
