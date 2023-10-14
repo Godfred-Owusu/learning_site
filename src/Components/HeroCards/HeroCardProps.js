cd620450fbec1cbde8a6993d6e0df64dbd55b751
Kobby7777 <godfredmirekuowusu@gmail.com>
2023-10-14 04:13:20 +0100

    resized the images

--- src/Components/HeroCards/HeroCardProps.js
+++ src/Components/HeroCards/HeroCardProps.js
@@ -3,7 +3,7 @@ import './HeroCard.css'
 export const HeroCardProp = (prop) => {
   return (
     <div className=''>
-    <div className={`mt-20 card-container rounded-lg`}>
+    <div className={`mt-20 card-container rounded-lg `}>
 
     <div>
         <img className={`w-10 h-10 ${prop.imageStyle} p-1 rounded`} src={prop.image} alt=''/>
--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -6,7 +6,7 @@ export const Pricing = () => {
     <section className='paddings w-full'>
       <p className='text-brand-orange font-bold my-5'>Practice Advice</p>
       <p className='font-bold text-xl'>Watch our Courses</p>
-      <p className='w-1/2 font-light my-5'>Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
+      <p className='lg:w-1/2 md:w-full sm:w-full font-light my-5'>Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
     <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 w-full'>
       <PricingProps new='hidden'/>
       <PricingProps/>
